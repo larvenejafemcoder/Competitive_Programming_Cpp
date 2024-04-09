1f025c56d34f4817ef5b7d108fe575de08e963f3
itzaRinna <dutchismahdick@gmail.com>
2024-04-09 16:14:48 +0700

    new file

---
+++ Nguyen Van Li/TamGiac.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int a[100000],n,x,y;
+void Input()
+{
+
+}
+
+void StamGiac(int j, int i, int k)
+{
+
+}
+
+void Abso
+
+void Process()
+{
+	for(int i=1; i<=n-2; i++)
+	{
+		for(int j=i+1; i<=n-1; j++)
+		{
+			for(int k=k+j; k<=n; k++)
+			{
+				if(StamGiac(i,j,k)>0)
+				{
+					if(StamGiac(i,j,k) == StamGiac(1,j,k)+StamGiac(1,j,i)+StamGiac(1,i,k))
+					{
+
+					}
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	
+}
