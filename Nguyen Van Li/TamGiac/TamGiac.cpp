063d026fc0f483b3a52c1e10fa340b5150d581e1
itzaRinna <dutchismahdick@gmail.com>
2024-04-13 20:54:38 +0700

    code moi cho tamgiac folder

--- Nguyen Van Li/TamGiac/TamGiac.cpp
+++ Nguyen Van Li/TamGiac/TamGiac.cpp
@@ -1,52 +1,62 @@
-#include <iostream>
-#include <fstream>
+#include <bits/stdc++.h>
 
 using namespace std;
-int a[100000],n,x,y;
+
+const int MAX_N = 100;
+const int MAX_SOTAMGIAC = 10000;
+
+int A[MAX_N][2];
+float B[MAX_SOTAMGIAC];
+int n;
 
 void Input()
 {
-	ifstream ip("TamGiac.inp");
-	ip>>x>>y;
-	ip.close();
+   	ifstream fin("TamGiac.inp");
+    fin >> n;
+    for (int i = 0; i < n; i++)
+        fin >> A[i][0] >> A[i][1];
+    fin.close();
 }
 
-void StamGiac(int j, int i, int k)
-{
-
+float area(int i, int j, int k) {
+    return abs((A[i][1] + A[j][1]) * (A[i][0] - A[j][0]) +
+               (A[j][1] + A[k][1]) * (A[j][0] - A[k][0]) +
+               (A[k][1] + A[i][1]) * (A[k][0] - A[i][0])) / 2.0;
 }
 
-void Abso()
+void Process() 
 {
-
-}
-
-void Process()
-{
-	for(int i=1; i<=n-2; i++)
-	{
-		for(int j=i+1; i<=n-1; j++)
-		{
-			for(int k=k+j; k<=n; k++)
-			{
-				if(StamGiac(i,j,k)>0)
-				{
-					if(StamGiac(i,j,k) == StamGiac(1,j,k)+StamGiac(1,j,i)+StamGiac(1,i,k))
-					{
-						
-					}
-				}
-			}
-		}
-	}
+    int p = 0;
+    for (int i = 0; i < n - 2; i++) 
+    {
+        for (int j = i + 1; j < n - 1; j++) 
+        {
+            for (int k = j + 1; k < n; k++) 
+            {
+                float S = area(i, j, k);
+                if (S > 0 && area(1, i, j) + area(1, k, i) + area(1, j, k) == S) 
+                {
+                B[p++] = S;
+                }
+            }
+        }
+    }
+    sort(B,B+p);
 }
 
 void Output()
 {
-	
+	ofstream out("TamGiac.out");
+    for (int i = 0; i < n; i++)
+    out << B[i] << " ";
+    out << endl;
+    out.close();
 }
 
 int main()
 {
-	
+	Input();
+	Process();
+	Output();
+	return 0;
 }
