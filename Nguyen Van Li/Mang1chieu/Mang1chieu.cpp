5653f37c3c291e9ccf64d39b7b89572e51c5015d
itzaRinna <dutchismahdick@gmail.com>
2024-04-30 09:41:00 +0700

    mang1chieunangcao thay li

--- Nguyen Van Li/Mang1chieu/Mang1chieu.cpp
+++ Nguyen Van Li/Mang1chieu/Mang1chieu.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int const AA = 1000000;
 int amin[2*AA+1], amax[2*AA+1]; // 2*10**6 + 1 với 2*10**6 + 1 là số phần tử có thể có trong array
-int dmax;
 int x,n,k;
 int cnt = 0;
 
@@ -14,7 +13,7 @@ void Input()
     ifstream ip("test.inp");
     ip >> n;
     ip >> k;
-    for (int i = 1;i < 1;i++)
+    for (int i = 1;i <= n;i++)
     {
         ip >> x;
         amax[x+AA] = i;
@@ -27,13 +26,32 @@ void Input()
         {
             amax[x+AA] = i;
         }
+        if(x+AA+k<AA*2+1 && x+AA-k > 0)
+        {
+            if (amax[AA + x + k] > 0) 
+            {
+                cnt = max(cnt, amax[AA + x + k] - amin[AA + x]);
+            }
+            if (amin[AA + x - k] > 0) 
+            {
+                cnt = max(cnt, amax[AA + x] - amin[AA + x - k]);
+            }
+    
+        }
 
     }
 
 }
 
+void Output() {
+    ofstream op("test.oup");
+    op << cnt;
+    op.close();
+}
+
 int main()
 {
-    
+    Input();
+    Output();
     return 0;
 }
